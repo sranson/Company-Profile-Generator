d8110cc90e011090093f27dd46a84e9be1f63a1d
Salome Ranson <salomeranson@gmail.com>
2021-04-19 18:22:05 -0400

    successfully adding all employee type to cards and rendering in HTML

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const { Manager, Engineer, Intern } = require('./classes');
 
 
+let employeesArray = []
+let cardsArray = []
+
 const getManagerInfo = function() {
     inquirer.prompt([
     {
@@ -29,8 +32,8 @@ const getManagerInfo = function() {
   .then(answers => {
     const {managerName, managerID, managerEmail, managerOfficeNumber } = answers;
     let manager = new Manager('Manager', managerName, managerID, managerEmail, managerOfficeNumber);  
-    manager.addHTML('Manager', manager.name, manager.ID, manager.email, manager.phoneNumber)
-    // console.log(manager.name, manager.ID, manager.email, manager.phoneNumber)
+    employeesArray.push(manager);
+    // manager.addHTML('Manager', manager.name, manager.ID, manager.email, manager.phoneNumber)
     addEmployeeMenu()
   })
   .catch(error => {
@@ -56,7 +59,8 @@ const getManagerInfo = function() {
         if (employeeType === 'Engineer' || employeeType === 'Intern') {
             getEmployeeInfo(employeeType);
         } else {
-            console.log(`The manager does NOT want to add an additional employee`);
+            console.log('Please find your company profile at index.html');
+            generateHTML(employeesArray);
         }
       })
       .catch(error => {
@@ -96,8 +100,9 @@ const getManagerInfo = function() {
                         },
                     ]).then(function(data) {
                         userName = data.userName;
-                        let engineer = new Engineer(employeeType, employeeName, employeeID, employeeEmail, userName);  
-                        engineer.addHTML('Engineer', engineer.name, engineer.ID, engineer.email, userName);
+                        let engineer = new Engineer(employeeType, employeeName, employeeID, employeeEmail, userName); 
+                        employeesArray.push(engineer);
+                        // engineer.addHTML('Engineer', engineer.name, engineer.ID, engineer.email, userName);
                         addEmployeeMenu()
                     })
                 } else if (employeeType === 'Intern') {
@@ -109,8 +114,9 @@ const getManagerInfo = function() {
                         },
                     ]).then(function(data) {
                         schoolName = data.schoolName;
-                        let intern = new Intern(employeeType, employeeName, employeeID, employeeEmail, schoolName);  
-                        intern.addHTML('Intern', intern.name, intern.ID, intern.email, schoolName );
+                        let intern = new Intern(employeeType, employeeName, employeeID, employeeEmail, schoolName); 
+                        employeesArray.push(intern); 
+                        // intern.addHTML('Intern', intern.name, intern.ID, intern.email, schoolName );
                         addEmployeeMenu()
                     })
                 }
@@ -126,6 +132,75 @@ const getManagerInfo = function() {
   }
 
 
+  function generateHTML(employeesArray) {
+    const htmlPageContent = generateCards(employeesArray)
 
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+         err ? console.log(err) : console.log('Successfully added to index.html!')
+    );
+  }
+
+
+  function generateCards(employeesArray) {
+    console.log(`There are ${employeesArray.length} in the Employees Array`);
+    employeesArray.forEach(element => {
+      if (element.phoneNumber) {
+        label = 'Phone Number';
+        myVar = element.phoneNumber
+      } else if (element.github) {
+        label = 'GitHub'
+        myVar = element.github
+      } else if (element.school) {
+        label = 'School'
+        myVar = element.school
+      }
+      card = `
+      <div class="col-md-3">
+      <div class="card" style="width: 18rem;">
+          <div class="card-body cardHeader">
+              <h5 class="card-title">${element.name}</h5>
+              <h5>${element.type}</h5>
+          </div>
+          <div class="addPadding">
+              <ul class="list-group">
+                  <li class="list-group-item">Employee ID: ${element.ID}</li>
+                      <li class="list-group-item">Email: ${element.email}</li>
+                      <li class="list-group-item">${label}: ${myVar}</li>
+                  </ul>
+          </div>
+      </div>
+  </div>
+        `
+    cardsArray.push(card);
+    })
+
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- CSS only -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+            <link rel="stylesheet" href="styles.css">
+            <title>Company Profile</title>
+        </head>
+        <body>
+            <header>
+                <div class="headerClass">
+                    <h1 class="headerText">Company Profile</h1>
+                </div>
+            </header>
+        
+            <!------------------------------------------------ TOP CARD ROW ------------------------------------------>
+            <div id="cardSection" class="CardSection">
+                    <!-- APPEND CARD TO THIS SECTION -->
+                ${cardsArray}
+            </div>
+        </body>
+      </html>
+    `
+  }
 
   getManagerInfo();
--- classes.js
+++ classes.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 
 
-
 class Employee {
     constructor(type, name, ID, email) {
         this.type = type;
@@ -18,13 +17,6 @@ class Manager extends Employee {
         super(type, name, ID, email)
         this.phoneNumber = phoneNumber;
     }
-    addHTML(type, name, ID, email, phoneNumber) {
-        const htmlPageContent = createHTML(type, name, ID, email, phoneNumber)
-
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-             err ? console.log(err) : console.log('Successfully added to index.html!')
-        );
-    }
 }
 
 
@@ -35,13 +27,6 @@ class Engineer extends Employee {
         super(type, name, ID, email);
         this.github = github;
     }
-    addHTML(type, name, ID, email, github) {
-        const htmlEngrContent = addEngineerToHTML(type, name, ID, email, github)
-
-        fs.appendFile('index.html', htmlEngrContent, (err) =>
-            err ? console.log(err) : console.log('Successfully added to index.html!')
-        );
-    }
 }
 
 
@@ -51,110 +36,7 @@ class Intern extends Employee {
         super(type, name, ID, email);
         this.school = school;
     }
-    addHTML(type, name, ID, email, school) {
-        const htmlInternContent = addInternToHTML(type, name, ID, email, school)
-
-        fs.appendFile('index.html', htmlInternContent, (err) =>
-            err ? console.log(err) : console.log('Successfully added to index.html!')
-        );
-    }
 }
-
-
-function createHTML(type, name, ID, email, variable) {
-    console.log(`The card type should be for: ${type}`);
-
-    let managerCard = `
-    <div class="col-md-3" id="card1>
-        <div class="card" style="width: 18rem;">
-            <div class="card-body cardHeader">
-                <h5 class="card-title">${name}</h5>
-                <h5>${type}</h5>
-            </div>
-            <div class="addPadding">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${ID}</li>
-                        <li class="list-group-item">Email: ${email}</li>
-                        <li class="list-group-item">Office number: ${variable}</li>
-                    </ul>
-            </div>
-        </div>
-    </div>
-    `
-    let initialHTMLpg = `
-    <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <!-- CSS only -->
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-            <link rel="stylesheet" href="styles.css">
-            <title>Company Profile</title>
-        </head>
-        <body>
-            <header>
-                <div class="headerClass">
-                    <h1 class="headerText">Company Profile</h1>
-                </div>
-            </header>
-        
-            <!------------------------------------------------ TOP CARD ROW ------------------------------------------>
-            <div id="cardSection" class="CardSection">
-                    <!-- APPEND CARD TO THIS SECTION -->
-                    ${managerCard}
-            </div>
-        </html>
-    </body>
-`
-
-return initialHTMLpg;
-}
-
-
-function addEngineerToHTML (type, name, ID, email, variable) {
-    let engineerCard = `
-    <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body cardHeader">
-                <h5 class="card-title">${name}</h5>
-                <h5>${type}</h5>
-            </div>
-            <div class="addPadding">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${ID}</li>
-                        <li class="list-group-item">Email: ${email}</li>
-                        <li class="list-group-item">GitHub: ${variable}</li>
-                    </ul>
-            </div>
-        </div>
-    </div>
-    `
-    return engineerCard;
-}
-
-function addInternToHTML (type, name, ID, email, variable) {
-    let internCard = `
-    <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body cardHeader">
-                <h5 class="card-title">${name}</h5>
-                <h5>${type}</h5>
-            </div>
-            <div class="addPadding">
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${ID}</li>
-                        <li class="list-group-item">Email: ${email}</li>
-                        <li class="list-group-item">School: ${variable}</li>
-                    </ul>
-            </div>
-        </div>
-    </div>
-    `
-    return internCard;
-}
-
 
 
 module.exports = {
